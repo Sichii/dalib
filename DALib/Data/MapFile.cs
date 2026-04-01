6fc785379c6cca0808e2411d14bcd37b4617db66
sichii <despereaux@live.com>
2026-04-01 14:55:20 -0400

    mapfile fix again

--- DALib/Data/MapFile.cs
+++ DALib/Data/MapFile.cs
@@ -39,9 +39,9 @@ public sealed class MapFile(int width, int height) : ISavable
         for (var y = 0; y < Height; ++y)
             for (var x = 0; x < Width; ++x)
             {
-                var background = reader.ReadUInt16();
-                var leftForeground = reader.ReadUInt16();
-                var rightForeground = reader.ReadUInt16();
+                var background = reader.ReadInt16();
+                var leftForeground = reader.ReadInt16();
+                var rightForeground = reader.ReadInt16();
 
                 Tiles[x, y] = new MapTile
                 {
@@ -142,15 +142,15 @@ public sealed class MapTile
     ///     The id of the background part of the tile. This id references a <see cref="Tile" /> from a <see cref="Tileset" />
     ///     loaded from Seo.dat
     /// </summary>
-    public ushort Background { get; init; }
+    public short Background { get; init; }
 
     /// <summary>
     ///     The id of the left foreground part of the tile. This id references an HPF image loaded from ia.dat
     /// </summary>
-    public ushort LeftForeground { get; set; }
+    public short LeftForeground { get; set; }
 
     /// <summary>
     ///     The id of the right foreground part of the tile. This id references an HPF image loaded from ia.dat
     /// </summary>
-    public ushort RightForeground { get; set; }
+    public short RightForeground { get; set; }
 }
